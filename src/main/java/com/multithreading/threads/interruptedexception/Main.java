2f401c5ed745999843aeb78ce6c477a826d65a2b
Eugene Sazonov <saz.vit@gmail.com>
2022-03-27 00:40:29 +0100

    Threads interrupted exception example

---
+++ src/main/java/com/multithreading/threads/interruptedexception/Main.java
@@ -0,0 +1,21 @@
+package com.multithreading.threads.interruptedexception;
+
+public class Main {
+	public static void main(String[] args) {
+		Thread thread = new Thread(new BlockingTask());
+
+		thread.start();
+		thread.interrupt();
+	}
+
+	private static class BlockingTask implements Runnable {
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(500000L);
+			} catch (InterruptedException e) {
+				System.out.println("Exiting blocking thread");
+			}
+		}
+	}
+}
